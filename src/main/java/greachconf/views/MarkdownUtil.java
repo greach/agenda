d1c9f042ebb53d5e469f8fa93b852a4713ae99ec
Sergio del Amo <sergio.delamo@softamo.com>
2019-03-23 19:09:08 +0100

    Markdown Processor

---
+++ src/main/java/greachconf/views/MarkdownUtil.java
@@ -0,0 +1,17 @@
+package greachconf.views;
+
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.options.MutableDataSet;
+
+class MarkdownUtil {
+
+    static String htmlFromMarkdown(String markdown) {
+        MutableDataSet options = new MutableDataSet();
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
+}
---
+++ src/main/java/greachconf/views/MarkdownViewModelProcessor.java
@@ -0,0 +1,59 @@
+package greachconf.views;
+
+import greachconf.vm.Speaker;
+import greachconf.vm.Talk;
+import io.micronaut.core.beans.BeanMap;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.views.ModelAndView;
+import io.micronaut.views.model.ViewModelProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Singleton
+public class MarkdownViewModelProcessor implements ViewModelProcessor {
+    private static final Logger LOG = LoggerFactory.getLogger(MarkdownViewModelProcessor.class);
+
+    public static final String KEY_TALK = "talk";
+    public static final String KEY_ABOUT = "about";
+    public static final String KEY_SPEAKER = "speaker";
+    public static final String KEY_BIO = "bio";
+
+    @Override
+    public void process(@Nonnull HttpRequest<?> request, @Nonnull ModelAndView<Map<String, Object>> modelAndView) {
+        modelAndView.getModel().ifPresent(viewModel -> {
+            if (viewModel.containsKey(KEY_TALK) &&
+                    viewModel.get(KEY_TALK) instanceof Talk) {
+                Map<String, Object> m = new HashMap<>(BeanMap.of(((Talk) viewModel.get(KEY_TALK))));
+
+                List<String> about = ((Talk) viewModel.get(KEY_TALK)).getAbout();
+                m.put(KEY_ABOUT, about.stream()
+                        .map(MarkdownUtil::htmlFromMarkdown)
+                        .collect(Collectors.toList()));
+                viewModel.replace(KEY_TALK, m);
+            }
+            if (viewModel.containsKey(KEY_SPEAKER) &&
+                    viewModel.get(KEY_SPEAKER) instanceof Speaker) {
+                Map<String, Object> m = new HashMap<>(BeanMap.of(((Speaker) viewModel.get(KEY_SPEAKER))));
+
+                List<String> bio = ((Speaker) viewModel.get(KEY_SPEAKER)).getBio();
+
+
+                LOG.trace("bio: {}", bio != null ? String.join(". ", bio) : "bio is null");
+                if (bio != null) {
+                    m.put(KEY_BIO, bio.stream()
+                            .map(MarkdownUtil::htmlFromMarkdown)
+                            .collect(Collectors.toList()));
+                }
+
+                viewModel.replace(KEY_SPEAKER, m);
+            }
+        });
+    }
+}
